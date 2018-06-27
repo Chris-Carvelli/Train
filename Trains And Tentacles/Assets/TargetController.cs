636f614136fb0607250e896253661eac00178b3d
Mathies Wiencke <mathieswiencke@hotmail.com>
2018-06-27 22:04:43 +0200

    Simple waypoint system so train follows target

---
+++ Trains And Tentacles/Assets/TargetController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+    public Transform[] wayPoints;
+
+    public float speed = 10f;
+
+    private int current;
+
+    // Update is called once per frame
+    void Update () {
+        if(transform.position != wayPoints[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, wayPoints[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        } else {
+            current = (current + 1) % wayPoints.Length;
+        }
+    }
+}
