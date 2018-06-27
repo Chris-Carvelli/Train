a1a0d50a71b0d2c58b0e9116c1da417f24892a8c
Christian Carvelli <chca@itu.dk>
2018-06-27 21:56:55 +0200

    Robot IK, first version (need to be fixed)

---
+++ Trains And Tentacles/Assets/Scripts/RobotArm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RobotJoint))]
+public class RobotArm : MonoBehaviour {
+	public Transform target;
+
+	//TMP make private
+	private RobotJoint[] _joints;
+	public float[] _angles;
+
+	// Use this for initialization
+	void Start () {
+		_joints = GetComponentsInChildren<RobotJoint>();
+		_angles = new float[_joints.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (RobotJoint joint in _joints)
+			joint.InverseKinematic(target.position, ref _angles);
+	}
+}
