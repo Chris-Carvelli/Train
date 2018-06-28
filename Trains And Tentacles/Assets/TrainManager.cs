e4f69c5bde5613cae44e8797153e50a11e247b0d
Mathies Wiencke <mathieswiencke@hotmail.com>
2018-06-28 14:41:22 +0200

    CartPrefabs created

---
+++ Trains And Tentacles/Assets/TrainManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainManager : MonoBehaviour {
+
+    [Range(1,5)]
+    public int health;
+
+    public GameObject cartPrefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i=1; i<health; i++)
+        {
+            //Instantiate new carts
+            GameObject cart = Instantiate(cartPrefab);
+            cart.transform.SetParent(this.transform);
+            cart.transform.position = transform.position - new Vector3(0,0,3*i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
